74796e8b934ba9e175567ca0370cb71fe2a60bb2
kjendrzyca <kjendrzyca@gmail.com>
2019-12-08 17:14:24 +0100

    Reorganize tests in module 1

--- src/01/02.db.test.js
+++ src/01/02.db.test.js
@@ -1,46 +1,4 @@
-import dbModule from './01.dbModule'
-
-describe('dbModule', () => {
-  it(`should connect to a database when connection string is provided`, () => {
-    // given
-    const connectionString = 'db://localhost:3000'
-
-    // when
-    const testDb = dbModule.connect(connectionString)
-
-    // then
-    expect(typeof testDb.collection).toEqual('function')
-  })
-
-  it(`should not connect to a database when connection string is not provided`, () => {
-    // given
-    // no connection string provided
-
-    // when
-    expect(() => dbModule.connect())
-      // then
-      .toThrow('invlid connection string!')
-  })
-
-  it(`should close db connection`, () => {
-    // given
-    const testDb = dbModule.connect('db://localhost:3000')
-
-    // when
-    const closedMessage = testDb.close()
-
-    // then
-    expect(closedMessage).toEqual('db connection closed!')
-    expect(testDb.isConnectionClosed()).toEqual(true)
-
-    // 💡 Note: we have two assertions here
-    // 1. one that checks the message
-    // 2. one that checks if the `isConnectionClosed` property is `true`
-    // We use both of those assertions to test if the 'feature' is working.
-    // That's what it means to have "one coherent assertion".
-    // It doesn't mean there needs to be only one `expect` per test.
-  })
-})
+import dbModule from './dbModule'
 
 describe('db', () => {
   it(`should throw an error when trying to access collection without name`, () => {
