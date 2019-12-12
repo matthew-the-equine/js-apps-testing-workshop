5f9ba3d38a1789d1d1b23bb8c7c40deddd85f503
kjendrzyca <kjendrzyca@gmail.com>
2019-12-12 15:54:44 +0100

    Add mocking modules example

---
+++ src/02-mocking/06.mocking-modules.test.js
@@ -0,0 +1,27 @@
+import superagent from 'superagent'
+import temperatureService from './weatherForecast/temperatureService'
+
+jest.mock('superagent')
+
+test.skip('should fetch', async () => {
+  const logger = {
+    info: () => null,
+  }
+
+  const response = {
+    temperature: 44,
+  }
+  const agent = {
+    set: jest.fn().mockReturnThis(),
+    send: jest.fn().mockReturnValue({ body: response }),
+  }
+  superagent.post.mockReturnValue(agent)
+
+  // when
+  const data = await temperatureService({url: 'localhost', logger})
+    .getTemperature({ unit: 'C' })
+
+  // then
+  expect(data).toEqual(response)
+  expect(agent.send.mock.calls[0][0]).toEqual({ unit: 'C' })
+})
