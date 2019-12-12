b758ea28fcd608aeb0970acc08000b1f25bb3a01
kjendrzyca <kjendrzyca@gmail.com>
2019-12-12 19:13:04 +0100

    Add testing bugs exercise

---
+++ src/04-what-to-test/06.bugs.test.js
@@ -0,0 +1,15 @@
+const examCalculator = () => ({
+  sum: (a, b) => {
+    return a + b
+  },
+})
+
+// 🐛 Bug report:
+// When adding '12' and '13' it returns '1213'
+// Expected result: should return 25 as a string
+
+describe(`Bugs`, () => {
+  it(`should ...`, () => {
+    // 💪 Just do it
+  })
+})
