7570580c1d5fa43be15205046ebe1e9c223e8c82
kjendrzyca <kjendrzyca@gmail.com>
2019-12-12 16:28:31 +0100

    Add express test example

---
+++ src/02-mocking/07.express.test.js
@@ -0,0 +1,32 @@
+import bodyParser from 'body-parser'
+
+const runExpressApp = ({
+  port = 3678,
+}) => {
+  const express = require('express')
+  const app = express()
+
+  app.use(bodyParser.json())
+
+  app.get('/temperature', (req, res) => {
+    if (req.body.unit === 'C') {
+      return res.send({ temperature: 44 })
+    }
+
+    return res.send({ temperature: 111 })
+
+  })
+
+  app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+
+  return app
+}
+
+describe.skip('express app route', () => {
+  it(`should response with 44 when calling with 'C' unit`, () => {
+    // 💪 just do it
+  })
+  it(`should response with 111 when calling with 'F' unit`, () => {
+    // 💪 just do it
+  })
+})
---
+++ src/02-mocking/final/07.express.test.js
@@ -0,0 +1,51 @@
+import bodyParser from 'body-parser'
+
+const temperatureRoute = (req, res) => {
+  if (req.body.unit === 'C') {
+    return res.send({ temperature: 44 })
+  }
+
+  return res.send({ temperature: 111 })
+}
+
+const runExpressApp = ({
+  port = 3678,
+}) => {
+  const express = require('express')
+  const app = express()
+
+  app.use(bodyParser.json())
+
+  app.get('/temperature', temperatureRoute)
+
+  app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+
+  return app
+}
+
+describe.skip('express app route', () => {
+  it(`should response with 44 when calling with 'C' unit`, () => {
+    // given
+    const body = { unit: 'C' }
+
+    // when
+    temperatureRoute({ body }, {
+      send: response => {
+        // then
+        expect(response).toEqual({ temperature: 44 })
+      },
+    })
+  })
+  it(`should response with 111 when calling with 'F' unit`, () => {
+    // given
+    const body = { unit: 'F' }
+
+    // when
+    temperatureRoute({ body }, {
+      send: response => {
+        // then
+        expect(response).toEqual({ temperature: 111 })
+      },
+    })
+  })
+})
